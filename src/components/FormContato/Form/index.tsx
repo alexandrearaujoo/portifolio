4907f3bf4ff37d73cadb9c37451b549b5faa4c5d
Alexandre Araujo <alexandre01393@gmail.com>
2022-06-25 19:54:52 -0300

    feat: final adjusts

--- src/components/FormContato/Form/index.tsx
+++ src/components/FormContato/Form/index.tsx
@@ -1,12 +1,31 @@
+import { useState } from 'react';
 import { Container, Input, FormContainer, TextArea } from './styles';
 
 const Form = () => {
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [message, setMessage] = useState<string>('');
+
   return (
     <Container>
-      <FormContainer data-aos='fade-up'>
-        <Input type="text" placeholder="Nome" />
-        <Input type="email" placeholder="Email" />
-        <TextArea placeholder="Mensagem" />
+      <FormContainer data-aos="fade-up">
+        <Input
+          type="text"
+          placeholder="Nome"
+          value={name}
+          onChange={({ target }) => setName(target.value)}
+        />
+        <Input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={({ target }) => setEmail(target.value)}
+        />
+        <TextArea
+          placeholder="Mensagem"
+          value={message}
+          onChange={({ target }) => setMessage(target.value)}
+        />
         <button type="submit">Enviar</button>
       </FormContainer>
     </Container>
