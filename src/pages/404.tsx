f162fc57ef73bbe2d83057317fdb41102aae1a1a
Alexandre Araujo <alexandre01393@gmail.com>
2022-06-28 22:06:17 -0300

    feat: page 404 done

---
+++ src/pages/404.tsx
@@ -0,0 +1,9 @@
+import PageError from "../components/PageError"
+
+const Page404 = () => {
+  return (
+    <PageError />
+  )
+}
+
+export default Page404
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { Toaster } from 'react-hot-toast';
 import GlobalStyles from '../styles/global';
 import theme from '../styles/theme';
 
+
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
