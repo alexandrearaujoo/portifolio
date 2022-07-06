9f680f968c0ae41c54461f941bdfc5fcb56ad394
Alexandre Araujo <alexandre01393@gmail.com>
2022-07-05 21:42:28 -0300

    fix: fixing send email

--- src/components/FormContato/Form/index.tsx
+++ src/components/FormContato/Form/index.tsx
@@ -4,24 +4,23 @@ import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { sendContactMail } from '../../../services/sendMail';
 import toast from 'react-hot-toast';
+import emailjs from '@emailjs/browser';
 
 interface SendMailProps {
   name: string;
-  senderMail: string;
-  content: string;
+  email: string;
+  message: string;
 }
 
 const Form = () => {
+
   const contactSchema = yup.object().shape({
     name: yup
       .string()
       .min(2, 'Minimo de 2 caracteres')
       .required('Campo obrigatório'),
-    senderMail: yup
-      .string()
-      .email('Email inválido')
-      .required('Campo obrigatório'),
-    content: yup.string().required('Campo obrigatório')
+    email: yup.string().email('Email inválido').required('Campo obrigatório'),
+    message: yup.string().required('Campo obrigatório')
   });
 
   const {
@@ -33,9 +32,14 @@ const Form = () => {
     resolver: yupResolver(contactSchema)
   });
 
-  const onSubmit = async (data: SendMailProps) => {
+  const onSubmit = async (data: any) => {
     try {
-      await sendContactMail(data);
+      emailjs.send(
+        process.env.NEXT_PUBLIC_SERVICE_ID,
+        process.env.NEXT_PUBLIC_TEMPLATE_ID,
+        data,
+        process.env.NEXT_PUBLIC_KEY
+      );
       toast.success('Mensagem enviada');
       reset();
     } catch (error) {
@@ -53,12 +57,12 @@ const Form = () => {
         />
         <Input
           type="text"
-          placeholder={errors.senderMail?.message.toString() || 'Email'}
-          {...register('senderMail')}
+          placeholder={errors.email?.message.toString() || 'Email'}
+          {...register('email')}
         />
         <TextArea
-          placeholder={errors.content?.message.toString() || 'Mensagem'}
-          {...register('content')}
+          placeholder={errors.message?.message.toString() || 'Mensagem'}
+          {...register('message')}
         />
         <button type="submit">Enviar</button>
       </FormContainer>
